4098c0e3f2e15ab5ff4dbfa5cb6991abbbcfbc0f
Antony21p <ferggx@yandex.ru>
2026-03-13 09:27:12 +0300

    13032026_w1()

--- src/main/java/com/example/andreuapp/dto/GroupEditDto.java
+++ src/main/java/com/example/andreuapp/dto/GroupEditDto.java
@@ -1,14 +1,32 @@
 package com.example.andreuapp.dto;
 
+import lombok.Builder;
+import lombok.Data;
 
+
+@Data
+@Builder
 public class GroupEditDto {
 
+    private Integer id;
     private String number;
 
+
+    public GroupEditDto(Integer id, String number) {
+        this.id = id;
+        this.number = number;
+    }
 
     public GroupEditDto() {
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
     public String getNumber() {
         return number;
