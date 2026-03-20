418abe33b6e85394403d86a5168e7ff1f23d47fa
Antony21p <ferggx@yandex.ru>
2026-03-20 07:57:36 +0300

    20032026_0.1

--- src/main/java/com/example/andreuapp/controller/GroupController.java
+++ src/main/java/com/example/andreuapp/controller/GroupController.java
@@ -1,17 +1,20 @@
 package com.example.andreuapp.controller;
 
-import com.example.andreuapp.dto.GroupEditDto;
-import com.example.andreuapp.dto.GroupReadDto;
+//import com.example.andreuapp.dto.GroupEditDto;
+
 import com.example.andreuapp.service.GroupService;
 import com.example.andreuapp.service.StudentService;
+import org.openapitools.api.V1Api;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.openapitools.model.GroupReadDto;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/v1/groups")
-public class GroupController {
+//@RequestMapping("/v1/groups")
+public class GroupController implements V1Api {
 
     private final GroupService groupService;
 
@@ -19,26 +22,49 @@ public class GroupController {
         this.groupService = groupService;
     }
 
-    @GetMapping()
-    public List<GroupReadDto> findAllGroups() {
-        return groupService.findAll();
+    @Override
+    public ResponseEntity<List<org.openapitools.model.GroupReadDto>> findAllGroups() {
+        return ResponseEntity.ok(groupService.findAll());
     }
 
-    @GetMapping("/{id}")
-    public Optional<GroupReadDto> findGroupById(@PathVariable Integer id) {
-        return groupService.findById(id);
+//    @GetMapping("/{id}")
+//    public Optional<GroupReadDto> findGroupById(@PathVariable Integer id) {
+//        return groupService.findById(id);
+//
+//    }
+//
+//    @PostMapping()
+//    public GroupReadDto createOrUpdateGroup(@RequestBody GroupEditDto groupEditDto) {
+//        return groupService.save(groupEditDto);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public boolean deleteGroup (@PathVariable Integer id) {
+//        return groupService.delete(id);
+//    }
 
-    }
 
-    @PostMapping()
-    public GroupReadDto createOrUpdateGroup(@RequestBody GroupEditDto groupEditDto) {
-        return groupService.save(groupEditDto);
-    }
 
-    @DeleteMapping("/{id}")
-    public boolean deleteGroup (@PathVariable Integer id) {
-       return groupService.delete(id);
-    }
+//    @GetMapping()
+//    public List<GroupReadDto> findAllGroups() {
+//        return groupService.findAll();
+//    }
+//
+//    @GetMapping("/{id}")
+//    public Optional<GroupReadDto> findGroupById(@PathVariable Integer id) {
+//        return groupService.findById(id);
+//
+//    }
+//
+//    @PostMapping()
+//    public GroupReadDto createOrUpdateGroup(@RequestBody GroupEditDto groupEditDto) {
+//        return groupService.save(groupEditDto);
+//    }
+//
+//    @DeleteMapping("/{id}")
+//    public boolean deleteGroup (@PathVariable Integer id) {
+//       return groupService.delete(id);
+//    }
 
 //    @PostMapping("/io")
 //    public GroupEditDto test(@RequestBody(required = false) GroupEditDto groupEditDTO) {
