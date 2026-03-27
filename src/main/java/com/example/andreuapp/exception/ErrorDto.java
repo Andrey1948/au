8ab46bf204881be06e28bf06081e621a38d877d0
Antony21p <ferggx@yandex.ru>
2026-03-27 08:51:17 +0300

    27032026_1

---
+++ src/main/java/com/example/andreuapp/exception/ErrorDto.java
@@ -0,0 +1,4 @@
+package com.example.andreuapp.exception;
+
+public class ErrorDto {
+}
---
+++ src/main/java/com/example/andreuapp/exception/RestExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.andreuapp.exception;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+
+    public ResponseEntity<ErrorDto> MyExeptionHandler () {
+
+
+    }
+
+}
